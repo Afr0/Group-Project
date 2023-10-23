ab3c5aa94a2ee123b9a5fa8ec2e9c39f0b02d01a
Mats Vederhus <afr088@hotmail.com>
2023-10-24 00:04:36 +0200

    Integrated variable display size and placeholder argument for input element from Ingrid's code.

--- src/Helseflora/MainView.js
+++ src/Helseflora/MainView.js
@@ -3,19 +3,30 @@ export default class MainView {
 
     constructor() {
         this.#columnClass = document.getElementsByClassName("plantCategoriesColumn")[0];
+
+        //Adapted from Ingrid's code.
+        document.getElementById("btnSearch").addEventListener("click", function() {
+            localStorage.setItem("searchword", document.getElementById("txtSearch").value);
+            window.location.href = "side2.html";
+        });
     }
 
     populatePlantCategories(rowData) {
+        console.log("MainView.populatePlantCategories():");
+        console.log(rowData);
         for(let i = 0; i < rowData.length; i++)  {
-            let row = document.createElement("div");
-            row.classList.add("plantCategoriesRow"); //Creates a div class.
-            row.innerHTML = rowData[i].kategori;
-            row.addEventListener("click", function() {
-                window.location.href = "http://127.0.0.1:5500/Helseflora/plants.html?category=" + 
-                encodeURIComponent(rowData[i].kategori.toLowerCase());
-            });
+            if(rowData[i].name !== "Not categorized") {
+                //I'm sure there's a more elegant solution to this, but this works.
+                let row = document.createElement("div");
+                row.classList.add("plantCategoriesRow"); //Creates a div class.
+                row.innerHTML = rowData[i].name;
+                row.addEventListener("click", function() {
+                    window.location.href = "http://127.0.0.1:5500/Helseflora/plants.html?category=" + 
+                    encodeURIComponent(rowData[i].name.toLowerCase());
+                });
 
-            this.#columnClass.appendChild(row);
+                this.#columnClass.appendChild(row);
+            }
         }
     }
  }
