f4c4c9d5bf42d1e113cf060900b06c37bc207a2d
Mats Vederhus <afr088@hotmail.com>
2023-10-24 00:06:48 +0200

    Rejigged caching and fetching data in MainModel.

--- src/Helseflora/MainModel.js
+++ src/Helseflora/MainModel.js
@@ -6,21 +6,19 @@
         this.#cachedData = localStorage.getItem("plantDataCache");
     }
 
-    fetchData() {
+    async fetchData(url) {
       //Specifying the method isn't neccessary but since we'll have an oral exam in this,
       //let us remind ourselves that we're sending a GET request to the server.
       //Apparently the browser will handle the SSL handshake for us so
       //we don't have to worry about it, we can just use fetch as normal. Nice!
-      fetch("https://sukkergris.no/plantcategories/", {method: "GET"})
-         .then(response => {
-            if (!response.ok) 
-               throw Error(response.statusText);
+      let response = await fetch(url, { method: "GET" });
+      if (!response.ok) 
+         throw Error(response.statusText);
 
-            return response.json(); //fetch() returns a promise (asynchronously), which when resolved will contain JSON data.
-         }).then(data => { 
-            this.cacheData(data);
-            return data;
-         });
+      let data = await response.json();
+      this.cacheData(data);
+
+      return data;
     }
 
     cacheData(data) {
@@ -31,12 +29,19 @@
 
       //The local storage only supports strings, so stringify...
       localStorage.setItem("plantDataCache", JSON.stringify(cacheObject));
+
+      //Update the private field with the newly cached data.
+      this.#cachedData = JSON.stringify(cacheObject);
     }
 
     //THIS FUNCTION RETURNS NULL IF THE CACHE EXPIRED,
     //SO THE RESULT ALWAYS NEEDS TO BE CHECKED!
     //TODO: Return a CacheResult class...
     getCachedData() {
+      //Fetch the cached data from localStorage each time this method is
+      //called, to ensure we always have the latest data.
+      this.#cachedData = localStorage.getItem("plantDataCache");
+
       if(!this.#cachedData)
          return null;
 
@@ -47,6 +52,8 @@
       if((currentTime - cachedTime) <= this.#hour)
          return cache.data;
 
+      //Clear the cached data if it has expired.
+      this.#cachedData = null;
       return null;
     }
  }
