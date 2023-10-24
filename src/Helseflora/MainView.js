b21a74c671c724d110fbaefb21d160d363df35ec
Mats Vederhus <afr088@hotmail.com>
2023-10-24 22:46:46 +0200

    Sanity checked search input, and sanity checked response from server.

--- src/Helseflora/MainView.js
+++ src/Helseflora/MainView.js
@@ -5,9 +5,15 @@ export default class MainView {
         this.#columnClass = document.getElementsByClassName("plantCategoriesColumn")[0];
 
         //Adapted from Ingrid's code.
-        document.getElementById("btnSearch").addEventListener("click", function() {
-            localStorage.setItem("searchword", document.getElementById("txtSearch").value);
-            window.location.href = "side2.html";
+        document.getElementsByClassName("btnSearch")[0].addEventListener("click", function() {
+            let inputValue = document.getElementById("txtSearch").value;
+
+            if (/^[a-æøåA-ÆØÅ0-9\s]+$/i.test(inputValue) && inputValue !== "") { //Sanity check
+                localStorage.setItem("searchword", inputValue);
+                window.location.href = "side2.html";
+            }
+            else
+                throw Error("Invalid input detected!"); //TODO: handle error...
         });
     }
 
@@ -15,11 +21,13 @@ export default class MainView {
         console.log("MainView.populatePlantCategories():");
         console.log(rowData);
         for(let i = 0; i < rowData.length; i++)  {
+            //I'm sure there's a more elegant solution to this, but this works.
             if(rowData[i].name !== "Not categorized") {
-                //I'm sure there's a more elegant solution to this, but this works.
                 let row = document.createElement("div");
                 row.classList.add("plantCategoriesRow"); //Creates a div class.
-                row.innerHTML = rowData[i].name;
+                //We DO trust the server, but let's assume that someone could have manipulated the data
+                //(Man in The Middle Attack), so use textContent instead of innerHTML.
+                row.textContent = rowData[i].name;
                 row.addEventListener("click", function() {
                     window.location.href = "http://127.0.0.1:5500/Helseflora/plants.html?category=" + 
                     encodeURIComponent(rowData[i].name.toLowerCase());
